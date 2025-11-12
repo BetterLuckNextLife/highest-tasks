f57fe7a24b67b7f01d215cc8f8bdcdeca894b819
”Egor” <rykovegor020@gmail.com>
2025-11-13 00:29:21 +0300

    Настройка работы приложения с БД

--- app/main.py
+++ app/main.py
@@ -1,7 +1,17 @@
 from flask import Flask
 from werkzeug.security import generate_password_hash, check_password_hash
+import os
+
+from db import db, Card, User, Board
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("SQLALCHEMY_DATABASE_URI")
+db.init_app(app)
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
